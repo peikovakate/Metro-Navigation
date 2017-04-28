f7c1c1040963cfea2bece409becce8b06d992dec
peikovakate <peikovakate@gmail.com>
2017-04-28 20:14:08 +0300

    added to Station some properties

--- Metro Navigation/Sources/Model/Station.cs
+++ Metro Navigation/Sources/Model/Station.cs
@@ -8,5 +8,9 @@ namespace Metro_Navigation.Sources.Model
 {
     class Station
     {
+        public string Name { get; set; }
+        float XPosition { get; set; }
+        float YPosition { get; set; }
+        int Line { get; set; }
     }
 }
