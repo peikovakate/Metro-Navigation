5c239904a9bb8e088379a451ebb8e3de77722fb3
peikovakate <peikovakate@gmail.com>
2017-04-28 20:03:26 +0300

    done some cleaning

--- Metro Navigation/Sources/View/MainWindow.xaml.cs
+++ Metro Navigation/Sources/View/MainWindow.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Metro_Navigation.Sources.Model;
-using System.Reflection;
+﻿using System.Windows;
 
 namespace Metro_Navigation.Sources.View
 {
@@ -22,20 +7,12 @@ namespace Metro_Navigation.Sources.View
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string CONNECTIONS_PATH = "data/connections.csv";
-        private const string STATIONS_PATH = "data/stations.csv";
+        
 
         public MainWindow()
         {
             InitializeComponent();
-            Metro metro = new Metro();
 
-            string path = AppDomain.CurrentDomain.BaseDirectory;
-
-            metro.ConnectionsSrc = path + CONNECTIONS_PATH;
-            metro.NamesSrc = path + STATIONS_PATH;
-            metro.LoadData();
-            metro.GO(7, 21);
         }
     }
 }
--- Metro Navigation/Sources/ViewModel/MainViewModel.cs
+++ Metro Navigation/Sources/ViewModel/MainViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Metro_Navigation.Sources.Model;
+
 
 namespace Metro_Navigation.Sources.ViewModel
 {
     class MainViewModel
     {
+        private const string CONNECTIONS_PATH = "data/connections.csv";
+        private const string STATIONS_PATH = "data/stations.csv";
+
+        public MainViewModel()
+        {
+            Metro metro = new Metro();
+
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            metro.ConnectionsSrc = path + CONNECTIONS_PATH;
+            metro.NamesSrc = path + STATIONS_PATH;
+            metro.LoadData();
+            metro.GO(7, 21);
+        }
     }
 }
