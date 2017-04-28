4943e3b66f3793948a3fba0095dd8f466f630a76
peikovakate <peikovakate@gmail.com>
2017-04-28 19:55:48 +0300

    implemented ICommand interface

---
+++ Metro Navigation/Sources/ViewModel/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Metro_Navigation.Sources.ViewModel
+{
+    public class Command : ICommand
+    {
+        public Command(Action<object> action)
+        {
+            ExecuteDelegate = action;
+        }
+
+        public Predicate<object> CanExecuteDelegate { get; set; }
+        public Action<object> ExecuteDelegate { get; set; }
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteDelegate != null)
+            {
+                return CanExecuteDelegate(parameter);
+            }
+
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (ExecuteDelegate != null)
+            {
+                ExecuteDelegate(parameter);
+            }
+        }
+    }
+}
